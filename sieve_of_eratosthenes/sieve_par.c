22acaa2088f84689ce35758fe79667f21ef92207
Devin Delfino <delfino_devin@wheatoncollege.edu>
2015-04-14 09:40:04 -0400

    finished commenting code for presentation

--- sieve_of_eratosthenes/sieve_par.c
+++ sieve_of_eratosthenes/sieve_par.c
@@ -17,50 +17,79 @@
 #define DONT_PRINT
 
 int main(int argc, char* argv[]) {
-	int procs;		// the number of processes
-	int rank;		// the rank of a given process
-	long range = 100;		// the upper bound of the range to find primes (2 to range)
-	double elapsed_time;
-	int local_prime_count;
-	long global_prime_count = 0;
+	int procs;						// the number of processes
+	int rank;						// the rank of a given process
+	long range = 100;				// the upper bound of the range to find primes (2 to range)
+	double elapsed_time;			// timekeeping
+	int local_prime_count;			// local prime count in a particular process
+	long global_prime_count = 0;	// total prime count 
 
 	MPI_Init(&argc, &argv);		// Initialize MPI, breaks into child processes -----------------------------------
 
 	if(argc > 1) {
 		range = atol(argv[1]); }
 
+	// ------------------------------------------------------------------
 	// declare helper functions so they are accessible by each process
 	long get_block_lowest(int,int,long);
 	long get_block_highest(int,int,long);
 	long get_block_size(int,int,long);
 	// long get_block_owner(int,int,long);
+	// ------------------------------------------------------------------
 
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);		// determines the rank of the current process of the communicator
-	MPI_Comm_size(MPI_COMM_WORLD, &procs);		// determine the total number of processes in the communicator
+	// ------------------------------------------------------------------
+	// MPI_Comm_rank()
+	// determines the rank of the current process
+	// Parameters:
+	// MPI_COMM_WORLD - object that provides the environment for message passing among processes
+	//          &rank - pass by reference, updates 'rank' to the rank of the current process
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
+	// MPI_Comm_size()
+	// determines the total number of processes
+	// Parameters:
+	// MPI_COMM_WORLD - object that provides the environment for message passing among processes
+	//          &procs - pass by reference, updates 'size' to the total number of processes
+	MPI_Comm_size(MPI_COMM_WORLD, &procs);
+
+	// MPI_Barrier()
+	// no process can proceed beyond a barrier until all processes have reached it
+	// ensures all processes enter the same block of code at the same time 
+	// Parameters:
+	// MPI_COMM_WORLD - object that provides the environment for message passing among processes
 	MPI_Barrier(MPI_COMM_WORLD);
-	elapsed_time = MPI_Wtime();
 
+	// MPI_Wtime()
+	// returns number of elapsed seconds
+	elapsed_time = MPI_Wtime();
+	// ------------------------------------------------------------------
+
+	// ------------------------------------------------------------------
 	// checks for too many processes - this will result in a higher need for communication, decreasing efficiency
-	int proc0_size = (range - 1)/procs;	// size of the 0th process
-	if( (proc0_size) < (int) sqrt( (double) range)) {
-		if(rank == 0) {
+	// ensures n/p > sqrt(n)
+	if(rank == 0) {
+		int proc0_size = (range - 1)/procs;	// size of the 0th process
+		if( (proc0_size) < (int) sqrt( (double) range)) {
 			printf("There are currently %d processes.\n", procs);
 			printf("If n = %ld, there must be less than %d processes in order to minimize communication.\n", range, (int)sqrt((double)range));
 		}
 		MPI_Finalize(); // exit MPI
 		exit(0);
 	}
+	// ------------------------------------------------------------------
 
-	long first = get_block_lowest(rank, procs, range - 1) + 2;	// gets the first number of the block
-	long last = get_block_highest(rank, procs, range - 1) + 2;	// gets the last number of the block
-	long size = get_block_size(rank, procs, range - 1);		// gets the size of the block
+	// ------------------------------------------------------------------
+	// parallelizing step 1 of sequential algorithm 
+	// 1. Create a list of natural numbers 2, 3, 4, ... , n, all marked 0
+
+	long first = get_block_lowest(rank, procs, range - 1) + 2;	// gets the first number represented by the block
+	long last = get_block_highest(rank, procs, range - 1) + 2;	// gets the last number represented by the block
+	long size = get_block_size(rank, procs, range - 1);			// gets the size of the block
 
 	printf("(Process %d) %ld - %ld, size %ld\n", rank, first, last, size);
 	fflush(stdout);
-	// parallelizing step 1 of sequential algorithm ----------------------------------
-	// 1. Create a list of natural numbers 2, 3, 4, ... , n, all marked 0
 
+	// Creates a block of the entire array to be handled by the current process
 	char* block;
 	block = (char *) malloc (size);
 	if(block == NULL) {
@@ -74,12 +103,16 @@ int main(int argc, char* argv[]) {
 	for(i=0; i < size; i++) {
 		block[i] = 0;
 	}
+	// ------------------------------------------------------------------
 
-	// parallelizing step 2 of sequential algorithm ----------------------------------
+	// ------------------------------------------------------------------
+	// parallelizing step 2 of sequential algorithm
 	// 2. Set k = 2, the first unmarked number on the list 
 	int current_prime = 2;
 	int current_squared = 4;
+	// ------------------------------------------------------------------
 	
+	// ------------------------------------------------------------------
 	// parallelizing step 3a of sequential algorithm ----------------------------------
 	// 3a. Mark all multiples of k between k^2 and n
 
@@ -88,7 +121,7 @@ int main(int argc, char* argv[]) {
 	int rem;		// the remainder of first % current_prime
 	int mark, it;   // iterators
 	while(current_squared <= range+1) {
-		// iterate through the block
+		// finding the first multiple of k in this block (greater than or equal to k^2)
 		if(current_squared <= first) { // k^2 already occurred, so we just mark the first multiple of the current prime
 			rem = first % current_prime;
 			if(rem == 0) {
@@ -108,6 +141,10 @@ int main(int argc, char* argv[]) {
 			block[mark] = 1;
 		}
 
+
+		// ------------------------------------------------------------------
+		// parallelizing step 3b of sequential algorithm ----------------------------------
+		// 3b. Set k to the smallest unmarked number greater than the current k
 		if(rank == 0) {
 			for(it = current_prime-2+1; it < size; it++) {
 				if(block[it] == 0) {
@@ -117,11 +154,25 @@ int main(int argc, char* argv[]) {
 			}
 		}
 
+		// MPI_Bcast()
+		// allows a process to broadast a data item to all other processes in the communicator (local communication)
+		// Parameters:
+		//  	    void *buffer - address of first element to be broadcasted
+		//             int count - number of elements to be broadcasted
+		// 	   MPI_Datatype type - type of element
+		// 			    int root - rank of process performing broadcast
+		// MPI_Comm communicator - communicator object
 		MPI_Bcast(&current_prime, 1, MPI_INT, 0, MPI_COMM_WORLD);
-
+		
 		current_squared = current_prime * current_prime;
-	}
+		// ------------------------------------------------------------------
 
+	}
+	// ------------------------------------------------------------------
+
+	// ------------------------------------------------------------------
+	// Iterates through the block, counts the unmarked values, 
+	// and increases the local prime count for the process
 	local_prime_count = 0;
 	for(it = 0; it < size; it++) {
 		if(block[it] == 0 ) {
@@ -132,21 +183,40 @@ int main(int argc, char* argv[]) {
 			#endif
 		}
 	}
+	// ------------------------------------------------------------------
 
-	// Reduction function to increase the global count of prime numbers stored in process 0
+	// ------------------------------------------------------------------
+	// Reduction function to increase the global count of 
+	// prime numbers stored in process 0
+
+	// MPI_Reduce()
+	// performs reduction operation on values submitted by all processes in a communicator (global communication)
+	// Parameters:
+	//  	   void *operand - address of first reduction element
+	// 			void *result - address of first reduction result
+	//             int count - number of reductions to perform
+	// 	   MPI_Datatype type - type of element
+	// 	     MPI_Op operator - reduction operator
+	// 			    int root - process receiving result
+	// MPI_Comm communicator - communicator object
 	MPI_Reduce(&local_prime_count, &global_prime_count, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
 	elapsed_time = MPI_Wtime() - elapsed_time;
 
 	MPI_Finalize();		// Terminates the MPI
+	// ------------------------------------------------------------------
 
+	// ------------------------------------------------------------------
+	// Print number of primes and elapsed time
 	if(rank==0) {
 		printf("----------------------------------------\n");
 		printf("Total primes between 2 and %ld: %ld\n", range, global_prime_count);
 		printf("Time Elapsed: %f seconds\n\n", elapsed_time);
 	}
+	// ------------------------------------------------------------------
 
 	return 0;
 }
+
 
 // ------ Helper Functions ------------------------------------------
 long get_block_lowest(int rank, int procs, long range) {
